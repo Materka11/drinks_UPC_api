f2d2ccb7c0a47ed6ee03ee2a56abb20da3d15616
Materka11 <arekmaterka11@gmail.com>
2024-09-06 11:36:50 +0200

    Feature: Get all categories

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using api.Data;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categories = _context.Categories.Select(c => c.toCategoryDto()).ToList();
+
+            return Ok(categories);
+        }
+    };
+}
+
+
---
+++ Mappers/CategoryMappers.cs
@@ -0,0 +1,18 @@
+﻿using api.Dtos.Category;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CategoryMappers
+    {
+        public static CategoryDto toCategoryDto(this Category categoryModel)
+        {
+            return new CategoryDto
+            {
+                Id = categoryModel.Id,
+                Name = categoryModel.Name,
+            };
+        }
+    }
+}
+
