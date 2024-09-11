31a9dad6eb86d7d830ad3eeec94927cf87dce2fb
Materka11 <arekmaterka11@gmail.com>
2024-09-11 11:12:22 +0200

    Feature: Create barcode request dto

---
+++ Dtos/Barcode/CreateBarcodeRequest.cs
@@ -0,0 +1,12 @@
+namespace api.Dtos.Barcode
+{
+    public class CreateBarcodeRequest
+    {
+        public long? UPC { get; set; }
+        public long? EAN { get; set; }
+        public long? ISBN { get; set; }
+        public long? JAN { get; set; }
+        public long? ITF_14 { get; set; }
+    }
+}
+}
