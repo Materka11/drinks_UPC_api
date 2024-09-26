09c9d28ce0e17cd7015d28a424975fea43e4ab29
Materka11 <arekmaterka11@gmail.com>
2024-09-26 10:25:41 +0200

    Feature: Create and update producer validate data

--- Dtos/Producer/CreateProducerRequest.cs
+++ Dtos/Producer/CreateProducerRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace api.Dtos.Producer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Producer
 {
     public class CreateProducerRequest
     {
+        [Required(ErrorMessage = "Producer name is required.")]
+        [StringLength(100, ErrorMessage = "Producer name cannot exceed 100 characters.")]
         public string Name { get; set; } = string.Empty;
     }
 }
