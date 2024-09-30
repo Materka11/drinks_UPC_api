c128ba3475abb22294987a4e170fd8000b7225b0
Materka11 <arekmaterka11@gmail.com>
2024-09-30 10:41:21 +0200

    Feature: Query object for get all drinks

---
+++ Queries/DrinkGetAllQuery.cs
@@ -0,0 +1,27 @@
+namespace api.Queries
+{
+    public class DrinkGetAllQuery
+    {
+        public string? DrinkName { get; set; } = null;
+
+        public string? BrandId { get; set; } = null;
+
+        public string? BrandName { get; set; } = null;
+
+        public string? ProducerId { get; set; } = null;
+
+        public string? ProducerName { get; set; } = null;
+
+        public string? Capacity { get; set; } = null;
+
+        public string? CategoryId { get; set; } = null;
+
+        public string? CategoryName { get; set; } = null;
+
+        public string? EAN { get; set; } = null;
+
+        public string? LabelId { get; set; } = null;
+
+        public string? LabelName { get; set; } = null;
+    }
+}
