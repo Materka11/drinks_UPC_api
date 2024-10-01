b0ba28ed849dbe9c0166796d6a7e1b0a7c121267
Materka11 <arekmaterka11@gmail.com>
2024-10-01 09:21:11 +0200

    Feature: Get all producers queries

--- Interfaces/IProducerRepository.cs
+++ Interfaces/IProducerRepository.cs
@@ -1,11 +1,12 @@
 using api.Dtos.Producer;
 using api.Models;
+using api.Queries;
 
 namespace api.Interfaces
 {
     public interface IProducerRepository
     {
-        Task<List<ProducerDto>> GetAllDtoAsync();
+        Task<List<ProducerDto>> GetAllDtoAsync(ProducerAllQuery query);
         Task<Producer?> GetByIdAsync(int id);
         Task<Producer> CreateAsync(Producer producerModel);
         Task<Producer?> UpdateAsync(int id, UpdateProducerRequest requestProducer);
