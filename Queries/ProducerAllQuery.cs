60fccbb0093d9f5cdf172fe39b566998b71f2e4a
Materka11 <arekmaterka11@gmail.com>
2024-10-02 10:44:14 +0200

    Feature: Sorting producers

--- Queries/ProducerAllQuery.cs
+++ Queries/ProducerAllQuery.cs
@@ -3,5 +3,9 @@ namespace api.Queries
     public class ProducerAllQuery
     {
         public string? ProducerName { get; set; } = null;
+
+        public string? SortBy { get; set; } = null;
+
+        public bool isDecsending { get; set; } = false;
     }
 }
